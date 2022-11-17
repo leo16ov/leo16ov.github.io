82ed3581142d0fe6cf38c2921ccb2306d669f8bd
Leo Ovando <leoxpro050@gmail.com>
2022-11-17 00:19:20 -0300

    agregando archivo js

---
+++ proyecto.js
@@ -0,0 +1,14 @@
+function saludar() {
+    alert("Bienvenido a Creaciones Mily. Somos un emprendimiento que se dedica a vender ropa, calzado, remeras de Mujer como de Hombre, de venta al Por Mayor y Por Menor.")
+    }
+
+function Nrowhatsapp(){
+    alert("Numero de Whatsapp: +54 9 11 7018-2763")
+}
+
+function modoscuro() {
+
+    var elemento = document.body;
+    elemento.classList.toggle("oscurecer");
+
+}
