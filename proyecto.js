a21b8f11a9a22b8ab717b363c1f8fba02c0dc0db
Leo Oviedo <leoxpro050@gmail.com>
2024-01-08 12:56:50 -0300

    se me olvido

--- proyecto.js
+++ proyecto.js
@@ -1,5 +1,5 @@
 function saludar() {
-    alert("Bienvenido a Creaciones Mily. Somos un emprendimiento que se dedica a vender ropa, calzado, remeras de Mujer como de Hombre, de venta al Por Mayor y Por Menor.")
+    alert("Bienvenido a Creaciones. Somos un emprendimiento que se dedica a vender ropa, calzado, remeras de Mujer como de Hombre, de venta al Por Mayor y Por Menor.")
     }
 
 function Nrowhatsapp(){
